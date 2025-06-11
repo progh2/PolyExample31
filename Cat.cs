dcd24105af626cae9b1ce7e2d17cf4c9bcc0a3ec
Gihun Ham <progh2@gmail.com>
2025-06-11 10:21:18 +0900

    Animal 클래스 상속 받도록 수정

--- Cat.cs
+++ Cat.cs
@@ -6,14 +6,8 @@ using System.Threading.Tasks;
 
 namespace PolyExample31
 {
-    internal class Cat
+    internal class Cat : Animal
     {
-        public int Age { get; set; }
-
-        public Cat() { this.Age = 0; }
-
-        public void Eat() { Console.WriteLine("냠냠 먹습니다."); }
-        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
         public void Meow() { Console.WriteLine("냥냥 웁니다."); }
     }
 }
